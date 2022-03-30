22ca5a70e118e4f3442350b93903a16000774496
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-03-30 12:27:44 +0530

    Added:Employee Present or Absent Logic.

---
+++ src/com/blz/javaproject/EmployeeWages.java
@@ -0,0 +1,29 @@
+package com.blz.javaproject;
+
+import java.util.Random;
+
+public class EmployeeWages {
+
+	public static void EmpWageUC1() {
+		Random ran = new Random();
+		int present = 1;
+		int empCheck = ran.nextInt(2);
+		if (empCheck == present) {
+			System.out.println("Employee is Present");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Employee Wage Computation Using OOP's Concept");
+		
+		EmpWageUC1();
+	
+	}
+	
+
+}
+
+
