c243e2e61e6b631579e4a8db007dec453c829486
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-03-30 11:58:57 +0530

    Added:Welcome to Employee Wages probelem Using OOPS Concept.

---
+++ src/com/blz/javaproject/Main.java
@@ -0,0 +1,9 @@
+package com.blz.javaproject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Employee Wages Computation Problem Using OOPS Concept");
+	}
+
+}
