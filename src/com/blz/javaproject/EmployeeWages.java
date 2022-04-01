93eab250695667011520e31ac7342c0f63c99e4b
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-01 08:41:08 +0530

    Added:Ability to save the total wage for Each Company

--- src/com/blz/javaproject/EmployeeWages.java
+++ src/com/blz/javaproject/EmployeeWages.java
@@ -1,45 +1,83 @@
 package com.blz.javaproject;
-
+import java.util.Random;
 public class EmployeeWages {
 
-	public static void calculatingEmployeeWage(String company,int empRatePerHour,int numberOfWorkingDays,int maxHoursPerMonth) {
-		int empWorkingHour=0;
-		int totalWorkingPerHour=0;
-		int totalWorkingDay=0;
+	static final int EMP_FULL_TIME = 1;
+	static final int EMP_PART_TIME = 2;
 
-		while(totalWorkingDay <= numberOfWorkingDays && totalWorkingPerHour < empRatePerHour) {
-			totalWorkingDay++;
-			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+	public String company;
+	public int wagePerHour;
+	public int workingDay;
+	public int totalWorkHrs;
 
-			switch(empCheck) {
-			case 1:
-				empWorkingHour = 8;
-				break;
-			case 2 :
-				empWorkingHour = 4;
-				break;
-			default : 
-				empWorkingHour = 0;
-				break;
-			}
-
-			totalWorkingPerHour+=empWorkingHour;
-			System.out.println("Day :"+totalWorkingDay);
-			System.out.println("Total Employee working hour  is  : " + empWorkingHour);
-
-		}
-		int totalEmployeewage = totalWorkingPerHour * empRatePerHour;
-		System.out.println("Total Employee Wage is : " + totalEmployeewage);
-		System.out.println("Total working hours are  : " + totalWorkingPerHour);
-
+	public EmployeeWages(String company, int wagePerHour, int workingDay, int totalWorkHrs) {
+		this.company = company;
+		this.wagePerHour = wagePerHour;
+		this.workingDay = workingDay;
+		this.totalWorkHrs = totalWorkHrs;
 	}
 
+	public void calculateEmpWage(){
+
+		int empWage = 0;
+		int totalWage = 0;
+		int totalWorkingHours = 0;
+		int totalWorkingDays = 0;
+
+		while (totalWorkingDays < workingDay && totalWorkingHours < totalWorkHrs) {
+
+			Random random = new Random();
+			int empPresent = random.nextInt(3);
+			System.out.println("Random Value for Employee Attendance is :" + empPresent);
+
+			int x;
+			switch (empPresent) {
+
+			case EMP_FULL_TIME:
+
+				x = wagePerHour * 8;
+				empWage = empWage + x;
+				totalWorkingHours = totalWorkingHours + 8;
+				System.out.println("Employee is present and the wage is : " + empWage);
+				totalWorkingDays++;
+				break;
+
+			case EMP_PART_TIME:
+				x = wagePerHour * 4;
+				empWage = empWage + x;
+				totalWorkingHours = totalWorkingHours + 4;
+				System.out.println("Employee is Part time present and the wage is : " + empWage);
+				totalWorkingDays++;
+				break;
+
+
+			default:
+				System.out.println("Employee is absent and the wage is : " + empWage);
+				break;
+
+			}
+
+		}
+
+		System.out.println("Total Working Days :" + totalWorkingDays);
+		System.out.println("Total Working Hours :" + totalWorkHrs);
+		System.out.println("Total Employee Wage for company " + company + " is :" + empWage);
+	}
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage calculation");
+		EmployeeWages comp1 = new EmployeeWages("COMP1", 40, 22, 140);
+		EmployeeWages comp2 = new EmployeeWages("COMP2", 90, 21, 222);
+		EmployeeWages comp3 = new EmployeeWages("COMP3", 58, 22, 175);
 
-		System.out.println("Tech Mahindra : ");
-		calculatingEmployeeWage("Tech Mahindra ", 120, 30, 10);
-		System.out.println("\nWipro : ");
-		calculatingEmployeeWage("Wipro ", 125, 8, 80);
+		comp1.calculateEmpWage();
+		System.out.println(comp1);
+		System.out.println("------------------------------------------------------------------");
+		comp2.calculateEmpWage();
+		System.out.println(comp2);
+		System.out.println("------------------------------------------------------------------");
+		comp3.calculateEmpWage();
+		System.out.println(comp3);
+		System.out.println("------------------------------------------------------------------");
 	}
 }
